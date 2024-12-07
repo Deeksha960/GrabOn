e9ab9260cb270c7676cb6ee2baebe9a2c698fed6
deeksha <deeksharao960@gmail.com>
2024-12-07 13:52:11 +0530

    commit

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,4 +1,4 @@
-import foodModel from "../models/FoodModel.js";
+import foodModel from "../models/foodModel.js";
 import fs from "fs";
 
 //add food item
@@ -11,7 +11,7 @@ const addFood = async (req, res) => {
     description: req.body.description,
     price: req.body.price,
     category: req.body.category,
-    image: image_filename
+    image: image_filename,
   });
   try {
     await food.save();
